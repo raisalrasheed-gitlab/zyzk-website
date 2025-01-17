6d8b900bc2dd830ac76b35189ae397dca1510527
Raisal Rasheed <raisalrasheed545@gmail.com>
2025-01-17 22:33:11 +0530

    Initial Setup Complete

---
+++ client/src/App.jsx
@@ -0,0 +1,10 @@
+import { Button } from '@material-tailwind/react';
+const App = () => {
+  return (
+    <>
+      <h1 className="bg-red-400">Hell</h1>
+      <Button>hello</Button>
+    </>
+  );
+};
+export default App;
